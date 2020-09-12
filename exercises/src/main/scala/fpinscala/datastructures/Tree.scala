dcd51fec4a72190394e2176ca662da66dec19550
Miguel Ping <miguel.ping@exoscale.ch>
2020-09-12 14:59:48 +0100

    Tree: add implementation

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,22 +7,37 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
-  def size[A](t: Tree[A]): Int = ???
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
 
-  def maximum(t: Tree[Int]): Int = ???
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
 
-  def depth[A](t: Tree[A]): Int = ???
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(v) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
 
-  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = ???
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 
-  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = ???
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+    case Leaf(v) => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
 
-  def sizeViaFold[A](t: Tree[A]): Int = ???
+  def sizeViaFold[A](t: Tree[A]): Int = fold(t)(_ =>1)((a,b) => 1 + a + b)
 
-  def maximumViaFold(t: Tree[Int]): Int = ???
+  def maximumViaFold(t: Tree[Int]): Int = fold(t)(v =>v)((a,b) => a max b)
 
-  def depthViaFold[A](t: Tree[A]): Int = ???
+  def depthViaFold[A](t: Tree[A]): Int = fold(t)(_ => 0)((a,b) => (1 + a max b))
 
-  def mapViaFold[A,B](t: Tree[A])(f: A => B): Tree[B] = ???
+  def mapViaFold[A,B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(v => Leaf(f(v)):Tree[B])((a,b) => Branch(a, b))
 
 }
